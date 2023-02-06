07d7b0c81da820d025e43dd7d8af0334f63cdb5a
alaajouri <alaa.joryi@hotmail.com>
2023-02-06 14:37:30 +0100

    Spring put post testen

--- src/main/java/com/example/hallospring/Controller/HalloWorld.java
+++ src/main/java/com/example/hallospring/Controller/HalloWorld.java
@@ -1,18 +1,38 @@
 package com.example.hallospring.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/API")
 public class HalloWorld {
     @GetMapping("/helloWorldSpring1")
-    public String helloworld1(){
+    public String helloworld1() {
         return "Hallo Spring1";
     }
+
     @GetMapping("/helloWorldSpring2")
-    public String helloworld2(){
+    public String helloworld2() {
         return "Hallo Spring2";
+    }
+
+    @GetMapping("/greating/{name}/{name2}")
+    public String greeting(@PathVariable String name, @PathVariable String name2) {
+        return "Hallo" + name + "and" + name2;
+
+    }
+    @GetMapping("/request")
+    public String greeting(@RequestParam String name) {
+        return "Hallo" + name ;
+
+    }
+
+    @GetMapping("/post")
+    public String post(@RequestBody String text) {
+        return "Body" + text ;
+
+    }
+
+    @GetMapping("/put")
+    public void put(@RequestBody product product) {
     }
 }
