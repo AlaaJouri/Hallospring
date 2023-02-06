fe9b17f6b3c95d1e6d7ecd2e900f78e04cfe3961
alaajouri <alaa.joryi@hotmail.com>
2023-02-06 12:55:13 +0100

    Spring

---
+++ src/main/java/com/example/hallospring/HalloSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.hallospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalloSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HalloSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hallospring/HalloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hallospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HalloSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
